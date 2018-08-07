e2efb75608bfdc5f25379aa6c5c675527a4f8f60
Gregory Shilin <gshilin@gmail.com>
2018-08-07 10:47:22 +0300

    tranzila J5-J4

--- lib/active_merchant/billing/gateways/tranzila.rb
+++ lib/active_merchant/billing/gateways/tranzila.rb
@@ -457,11 +457,14 @@ module ActiveMerchant #:nodoc:
       end
 
       def token_purchase_j4(money, options = {})
+        year   = options[:expyear].to_s[-2, 2]
         params = {
             :sum        => amount(money),
             :currency   => @options[:currency],
             :tranmode   => 'F',
             :TranzilaTK => options[:TranzilaTK],
+            :expdate    => "#{options[:expmonth]}#{year}",
+            :cred_type  => '1',
             :authnr     => options[:authnr],
             :index      => options[:index],
 
